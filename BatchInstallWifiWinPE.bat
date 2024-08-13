29ad894030e7178c13a49c73dc27d7d7fb80e582
PO-RCS <Polsen@rivercity.net.au>
2024-08-13 16:46:16 +1000

    Update BatchInstallWifiWinPE.bat

--- BatchInstallWifiWinPE.bat
+++ BatchInstallWifiWinPE.bat
@@ -1,2 +1,2 @@
-﻿start /min powershell.exe 'C:\Users\Administrator\Documents\GitHub\OSD-CloudTesting\Wifi Answer File.ps1'
-start /min powershell.exe 'C:\Users\Administrator\Documents\GitHub\OSD-CloudTesting\WinPE Wifi Pack.ps1'
+﻿start /min powershell.exe 'D:\OSD-OSDCloud\Wifi Answer File.ps1'
+start /min powershell.exe 'D:\OSD-OSDCloud\WinPE Wifi Pack.ps1'
