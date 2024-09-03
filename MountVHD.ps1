85c3b154d8648ec91cb7ea9e38cfbf0dc41dba6c
PO-RCS <Polsen@rivercity.net.au>
2024-09-03 13:05:53 +1000

    Update

---
+++ MountVHD.ps1
@@ -0,0 +1,13 @@
+﻿Mount-VHD -Path "C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\The Perfect Image - Copy.vhdx"
+
+# Get the disk number for the mounted VHDX
+$disk = Get-Disk | Where-Object { $_.Path -eq 'C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\The Perfect Image - Copy.vhdx' }
+
+# Get the partition on the disk
+$partition = Get-Partition -DiskNumber $disk.Number
+
+# Assign a drive letter (e.g., X)
+$partition | Set-Partition -NewDriveLetter H
+Get-Partition
+
+Dismount-VHD -Path "C:\ProgramData\Microsoft\Windows\Virtual Hard Disks\The Perfect Image - Copy.vhdx"
---
+++ RunbookCustom.ps1
@@ -0,0 +1,26 @@
+﻿$WindowsImage = "C:\Users\Administrator\Desktop\OSD-CloudTesting\Custom.wim"
+$Destination = "$(Get-OSDCloudWorkspace)\Media\OSDCloud\OS"
+New-Item -Path $Destination -ItemType Directory -Force
+Copy-Item -Path $WindowsImage -Destination "$Destination\CustomImage.wim" -Force
+New-OSDCloudISO
+
+Edit-OSDCloudWinPE -Wallpaper C:\Users\Administrator\Desktop\OSD-CloudTesting\RCSLogo.jpg -StartOSDCloudGUI -CloudDriver * 
+
+#Cleanup OSDCloud Workspace Media
+$KeepTheseDirs = @('boot','efi','en-us','sources','fonts','resources')
+Get-ChildItem 'C:\ProgramData\OSDCloud\Templates\Perfect Image\Media' | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem 'C:\ProgramData\OSDCloud\Templates\Perfect Image\Media\Boot' | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem 'C:\ProgramData\OSDCloud\Templates\Perfect Image\Media\EFI\Microsoft\Boot' | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+
+
+New-OSDCloudTemplate -Name 'Perfect Image'
+
+New-OSDCloudWorkspace -WorkspacePath 'C:\ProgramData\OSDCloud\Templates\Perfect Image'
+
+Get-OSDCloudTemplateNames
+
+Set-OSDCloudTemplate -Name 'Perfect Image'
+
+#Image location in WinPE D:\OSDCloud\OSD\CustomImage.wim
+
+
